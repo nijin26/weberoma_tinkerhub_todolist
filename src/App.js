87d06a1264125d1eb0b2e6666475c75ba6fbabea
nijin26 <nijinazarkpz@gmail.com>
2021-08-20 21:19:57 +0530

    Completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [input, setInput] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  const createTodo = (e) => {
+    e.preventDefault();
+    const newTodo = { id: new Date().getTime(), title: input };
+    setTodos((prev) => [...prev, newTodo]);
+  };
+
+  const deleteHandler = (id) => {
+    const newTodo = todos.filter((todo) => todo.id !== id);
+    setTodos(newTodo);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <h1>Weboroma Todo List Manager</h1>
+
+        <form onSubmit={createTodo}>
+          <input type="text" placeholder="Enter you todo" onChange={(e) => setInput(e.target.value)} />
+          <button type="submit"> Add </button>
+        </form>
+        <ul className="list">
+          {todos.map((todo) => (
+            <li key={todo.id}>
+              {" "}
+              {todo.title} <span onClick={() => deleteHandler(todo.id)}> X </span>{" "}
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
